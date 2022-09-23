32cc79701b47142e9a088ee0aa9d9f9b4edf06a1
Simba Zhang <solderzzc@gmail.com>
2022-09-23 12:59:51 -0700

    disable SIMD for aarch64

--- thirdparty/faiss/faiss/FaissHook.cpp
+++ thirdparty/faiss/faiss/FaissHook.cpp
@@ -6,8 +6,8 @@
 
 #include <faiss/FaissHook.h>
 #include <faiss/impl/ScalarQuantizerDC.h>
-#include <faiss/impl/ScalarQuantizerDC_avx.h>
-#include <faiss/impl/ScalarQuantizerDC_avx512.h>
+// #include <faiss/impl/ScalarQuantizerDC_avx.h>
+// #include <faiss/impl/ScalarQuantizerDC_avx512.h>
 
 namespace faiss {
 
@@ -25,29 +25,33 @@ void hook_init(std::string& simd_type) {
     std::lock_guard<std::mutex> lock(hook_mutex);
 
     // SQ8 always hook best SIMD
-#ifdef __linux__
-    if (faiss_use_avx512 && cpu_support_avx512()) {
-        /* for IVFSQ */
-        sq_get_distance_computer = sq_get_distance_computer_avx512;
-        sq_sel_quantizer = sq_select_quantizer_avx512;
-        sq_sel_inv_list_scanner = sq_select_inverted_list_scanner_avx512;
-    } else if (faiss_use_avx2 && cpu_support_avx2()) {
-        /* for IVFSQ */
-        sq_get_distance_computer = sq_get_distance_computer_avx;
-        sq_sel_quantizer = sq_select_quantizer_avx;
-        sq_sel_inv_list_scanner = sq_select_inverted_list_scanner_avx;
-    } else if (faiss_use_sse4_2 && cpu_support_sse4_2()) {
-        /* for IVFSQ */
-        sq_get_distance_computer = sq_get_distance_computer_ref;
-        sq_sel_quantizer = sq_select_quantizer_ref;
-        sq_sel_inv_list_scanner = sq_select_inverted_list_scanner_ref;
-    } else {
-        /* for IVFSQ */
-        sq_get_distance_computer = sq_get_distance_computer_ref;
-        sq_sel_quantizer = sq_select_quantizer_ref;
-        sq_sel_inv_list_scanner = sq_select_inverted_list_scanner_ref;
-    }
-#endif
+// #ifdef __linux__
+//     if (faiss_use_avx512 && cpu_support_avx512()) {
+//         /* for IVFSQ */
+//         sq_get_distance_computer = sq_get_distance_computer_avx512;
+//         sq_sel_quantizer = sq_select_quantizer_avx512;
+//         sq_sel_inv_list_scanner = sq_select_inverted_list_scanner_avx512;
+//     } else if (faiss_use_avx2 && cpu_support_avx2()) {
+//         /* for IVFSQ */
+//         sq_get_distance_computer = sq_get_distance_computer_avx;
+//         sq_sel_quantizer = sq_select_quantizer_avx;
+//         sq_sel_inv_list_scanner = sq_select_inverted_list_scanner_avx;
+//     } else if (faiss_use_sse4_2 && cpu_support_sse4_2()) {
+//         /* for IVFSQ */
+//         sq_get_distance_computer = sq_get_distance_computer_ref;
+//         sq_sel_quantizer = sq_select_quantizer_ref;
+//         sq_sel_inv_list_scanner = sq_select_inverted_list_scanner_ref;
+//     } else {
+//         /* for IVFSQ */
+//         sq_get_distance_computer = sq_get_distance_computer_ref;
+//         sq_sel_quantizer = sq_select_quantizer_ref;
+//         sq_sel_inv_list_scanner = sq_select_inverted_list_scanner_ref;
+//     }
+// #endif
+
+    sq_get_distance_computer = sq_get_distance_computer_ref;
+    sq_sel_quantizer = sq_select_quantizer_ref;
+    sq_sel_inv_list_scanner = sq_select_inverted_list_scanner_ref;
 
     hook_fvec(simd_type);
 }
